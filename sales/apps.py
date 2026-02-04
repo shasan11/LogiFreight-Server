7a469331b30f56a78bd9d0c4cdf928fc3be5adc2
Shasan Dhakal <63613238+shasan11@users.noreply.github.com>
2026-02-04 20:21:16 +0545

    Add sales approval signals for customer accounts

--- sales/apps.py
+++ sales/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class SalesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sales'
+
+    def ready(self) -> None:
+        from .signals import register_sales_signals
+
+        register_sales_signals()
