953ef130d9156588f09fb035b5a0bdc987dee272
Shasan Dhakal <63613238+shasan11@users.noreply.github.com>
2026-02-04 20:38:23 +0545

    Sync handling units with package quantity

--- operations/apps.py
+++ operations/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class OperationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'operations'
+
+
+    def ready(self) -> None:
+        from .signals import register_operations_signals
+
+        register_operations_signals()
