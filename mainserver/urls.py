7d8ba28dd417445187bba7259ce3e7ee66b2ab9d
Shasan Dhakal <shasandkl4@gmail.com>
2026-02-05 12:37:50 +0545

    Fixed the homepage

--- mainserver/urls.py
+++ mainserver/urls.py
@@ -9,26 +9,21 @@ from drf_spectacular.views import (
     SpectacularRedocView,
 )
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     path('accounting/', include('accounting.urls')),
     path('actors/', include('actors.urls')),
-    # path('authentication/', include('authentication.urls')),
-    # path('core/', include('core.urls')),
-    # path('crm/', include('crm.urls')),
+    path('authentication/', include('authentication.urls')),
+    path('core/', include('core.urls')),
+    path('crm/', include('crm.urls')),
      path('master/', include('master.urls')),
     path('operations/', include('operations.urls')),
-    # path('pickup/', include('pickup.urls')),
+    path('pickup/', include('pickup.urls')),
     path('purchase/', include('purchase.urls')),
     path('sales/', include('sales.urls')),
-    # path('warehouse/', include('warehouse.urls')),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-
-    # Swagger UI
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-
-    # Redoc UI (optional)
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    
+    path('warehouse/', include('warehouse.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),#the normal api docs path
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),# Swagger UI
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),   # Redoc UI (optional)
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
 ]
